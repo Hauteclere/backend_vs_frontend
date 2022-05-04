503c3942489458b02366ebd448f0add1466d9228
Hauteclere <oliver.f.lavers@gmail.com>
2022-05-04 14:35:30 +1000

    modified components

--- examplefrontend/src/components/refreshbutton.js
+++ examplefrontend/src/components/refreshbutton.js
@@ -1,8 +1,8 @@
-const RefreshButton = (props) => {
+const DarkModeButton = (props) => {
     return (
-        <button className='refresh-button' onClick={function() { props.getData(); }}>
-            Refresh List
+        <button className='dark-mode-button' onClick={function() { props.toggleDarkMode(); }}>
+            Toggle Dark Mode
         </button>
     );
 }
-export default RefreshButton;
+export default DarkModeButton;
--- examplefrontend/src/components/shoppinglist.js
+++ examplefrontend/src/components/shoppinglist.js
@@ -2,13 +2,18 @@ import { useEffect, useState } from "react";
 import RefreshButton from "./refreshbutton";
 
 const ShoppingList = (props) => {
+    
+    
     return (
         <div className='shopping-list'>
             <h1>Shopping List for Oliver</h1>
             <ul>
-                
+                <li>Onions</li>
+                <li>Garlic</li>
+                <li>Celery</li>
+                <li>Balsamic Vinegar</li>
             </ul>
-            <RefreshButton getData={getData}/>
+            <RefreshButton toggleDarkMode={toggleDarkMode}/>
         </div>
     );
     
